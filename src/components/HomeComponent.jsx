5ce98d24b7e713e8fb69cce1d35513dc295ad4c8
kushalpokharel <kusaalpokharel@gmail.com>
2022-02-03 13:07:43 +0545

    signing from issuer and extracting pubkey added

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -3,6 +3,7 @@ import {collection, getDocs} from 'firebase/firestore';
 import db from './firebase/firebase';
 import {Card, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {hashPersonalMessage, toBuffer, fromRpcSig, ecrecover,ecsign,publicToAddress,bufferToHex, fromUtf8} from 'ethereumjs-util';
 
 function Home(){
     const [snapshot, setSnapshot] = useState([]);
@@ -10,12 +11,12 @@ function Home(){
 
     useEffect(async()=>{
         
-            console.log("here");
+            // console.log("here");
             querySnapshot = await getDocs(collection(db, "request"));
             let snap = [];
             // console.log(querySnapshot);
             querySnapshot.forEach((doc) => {
-                console.log(doc.data().objects);
+                // console.log(doc.data().objects);
                 // console.log(snapshot);
                 // setSnapshot(snapshot.push({[doc.id] : doc.data().objects}))
                 snap.push({[doc.id]:doc.data().objects});
@@ -26,12 +27,36 @@ function Home(){
             });
             setSnapshot(snap)
     },[]);
-    console.log(snapshot);
+    // console.log(snapshot);
+
+    const handleClick = (object) => {
+            console.log("here")
+            Object.values(object).map(arrelem =>{
+            
+                arrelem.map( (arrel) => {
+                    console.log(Object.values(arrel)[0])
+                    let dat = fromUtf8(Object.values(arrel)[0]);
+                    let message = toBuffer(dat);
+                    let msgHash = hashPersonalMessage(message);
+                    let privateKey = new Buffer.from('556ea9f6abff39f133a43158e0860d543e7f7d7d186d24a2e8627e3dc803686e','hex');
+                    let sig = ecsign(msgHash, privateKey);
+                    console.log(sig)
+                    // let signature = toBuffer(sig);
+                    let sigParams = sig;
+                    let publicKey = ecrecover(msgHash, sigParams.v, sigParams.r, sigParams.s)
+                    let sender = publicToAddress(publicKey);
+                    let addr = bufferToHex(sender);
+                    bufferToHex(msgHash);
+                    console.log(msgHash, addr);
+                }
+            )
+        })
+    } 
     
     return (
         snapshot.map(object => 
             {
-                console.log(Object.values(object));
+                //console.log(Object.values(object));
                 return <div style={{display:'flex',justifyContent:'center'}}><Card style={{width:'55%',textAlign:'center', margin:'10px'}}>
                     <Card.Header style={{backgroundColor:'lightblue'}}>{Object.keys(object)}</Card.Header>
                     {
@@ -46,7 +71,7 @@ function Home(){
                     
                     }
                     <div style={{textAlign:'right'}}>
-                        <Button variant="primary" style={{margin:'2% 0% 2% 2%'}}>Accept</Button>
+                        <Button variant="primary" style={{margin:'2% 0% 2% 2%'}} onClick={() =>  handleClick(object)}>Accept</Button>
                         <Button variant="danger" style={{margin:'2% 8% 2% 2%'}}>Reject</Button>
                     </div>
                 </Card> 
